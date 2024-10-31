0cdf54492abf0eac5ff9a1fdf05a7e6c6b0bc3e1
Maximilianych <maxgorochov27@gmail.com>
2024-10-31 17:57:35 +0300

    add memory usage history

--- src/gui.rs
+++ src/gui.rs
@@ -3,13 +3,14 @@ use std::collections::VecDeque;
 use std::time;
 use sysinfo::{Disks, System};
 
-use crate::widgets;
+use crate::widgets::{self, memory};
 
 // WidgetApp
 pub struct WidgetApp {
     system: System,
     discs: Disks,
     cpu_usage_history: VecDeque<f32>,
+    memory_usage_history: VecDeque<f32>,
     frame_duration: time::Duration,
 }
 
@@ -20,6 +21,7 @@ impl Default for WidgetApp {
             system: System::new(),
             discs: Disks::new_with_refreshed_list(),
             cpu_usage_history: VecDeque::with_capacity(100),
+            memory_usage_history: VecDeque::with_capacity(100),
             frame_duration: time::Duration::from_secs_f64(1.0 / 60.0),
         }
     }
@@ -106,8 +108,8 @@ impl eframe::App for WidgetApp {
 
             // Cpu Usage Plot
             egui::Window::new("Cpu Usage Plot").show(ctx, |ui| {
-                let cpu_usage: f32 =
-                    widgets::cpu_usage(&mut self.system).iter().sum::<f32>() / self.system.cpus().len() as f32;
+                let cpu_usage: f32 = widgets::cpu_usage(&mut self.system).iter().sum::<f32>()
+                    / self.system.cpus().len() as f32;
                 self.cpu_usage_history.push_back(cpu_usage);
                 if self.cpu_usage_history.len() > 100 {
                     self.cpu_usage_history.pop_front();
@@ -127,7 +129,32 @@ impl eframe::App for WidgetApp {
                     .include_y(100.0)
                     .include_x(100.0)
                     .show(ui, |plot_ui| plot_ui.line(cpu_usage_line));
-            });            
+            });
+
+            // memory usage plot
+            egui::Window::new("Memory Usage Plot").show(ctx, |ui| {
+                self.system.refresh_memory();
+
+                self.memory_usage_history.push_back(self.system.used_memory() as f32 / 1_048_576.0);
+                if self.memory_usage_history.len() > 100 {
+                    self.memory_usage_history.pop_front();
+                }
+                
+                let memory_usage_points: Vec<_> = self
+                    .memory_usage_history
+                    .iter()
+                    .enumerate()
+                    .map(|(i, v)| [i as f64, *v as f64])
+                    .collect();
+                let memory_usage_line = Line::new(memory_usage_points);
+
+                Plot::new("memory_usage_plot")
+                    .view_aspect(3.0)
+                    .include_y(0.0)
+                    .include_y(self.system.total_memory() as f64 / 1_048_576.0)
+                    .include_x(100.0)
+                    .show(ui, |plot_ui| plot_ui.line(memory_usage_line));
+            });
         });
 
         // TODO: adequate frame limitation
